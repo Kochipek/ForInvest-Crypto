c875431a16cb8cc411bb678c05bd7a50a9827eb2
Ipek Kochisarli <ipekkochisarli@gmail.com>
2025-07-29 21:39:18 +0300

    Integrate Flipper for network debugging

--- app/src/main/java/com/ipekkochisarli/forinvest_crypto/core/Application.kt
+++ app/src/main/java/com/ipekkochisarli/forinvest_crypto/core/Application.kt
@@ -1,9 +1,26 @@
 package com.ipekkochisarli.forinvest_crypto.core
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.ipekkochisarli.forinvest_crypto.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
-class Application : Application(){
+import com.facebook.soloader.SoLoader
+import com.ipekkochisarli.forinvest_crypto.core.data.remote.FlipperHolder
 
+@HiltAndroidApp
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(FlipperHolder.networkFlipperPlugin)
+            client.start()
+        }
+    }
 }
---
+++ app/src/main/java/com/ipekkochisarli/forinvest_crypto/core/data/remote/FlipperHolder.kt
@@ -0,0 +1,7 @@
+package com.ipekkochisarli.forinvest_crypto.core.data.remote
+
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+
+object FlipperHolder {
+    val networkFlipperPlugin = NetworkFlipperPlugin()
+}
